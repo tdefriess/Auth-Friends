41521839c7d5c37b15c55659fea7215df2993dad
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-25 19:33:34 -0500

    Built axiosWithAuth in /utils, started login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login e => {
+        e.preventDefault();
+        axiosWithAuth()
+    }
+}
